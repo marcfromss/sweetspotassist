92fc0100efa94c28cea518e66136d0c975e76127
digital World <digitalworld@digitals-MacBook-Air.local>
2025-08-07 19:24:31 -0400

    Remove calling features, simplify document upload, and fix Next.js config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ['localhost'],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig 
